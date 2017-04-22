b5a7549faa19bdf7e6beb13fb95ab0988f885685
Jim Vang <vangjim15@gmail.com>
2017-04-22 14:28:54 -0500

    added ajax to client.js

--- public/client.js
+++ public/client.js
@@ -12,17 +12,42 @@ function onReady (){
 }
 
 function addButton(){
+  // checking if add button works when click
   console.log('add button click');
-}
+
+  // get the user input and package into an object
+  var objectToSend = {
+    num1Enter: $('#num1').val(),
+    num2Enter: $('#num2').val()
+  }; // end of objectToSend
+  console.log('sending ->', objectToSend);
+
+  //sending object to server.js via ajax
+  $.ajax({
+    url: '/addNum',
+    type: 'POST',
+    data: objectToSend,
+    success: function (response) {
+      console.log('back with the server with:', response);
+    }
+  });
+
+} // end of addButton function
 
 function subButton(){
+  // checking if sub button works when click
   console.log('sub button click');
-}
+
+} // end of subButton function
 
 function multButton(){
+  // checking if mult button works when click
   console.log('mult button click');
-}
+
+} // end of multButton function
 
 function divButton(){
+  // checking if div button works when click
   console.log('div button click');
-}
+
+} // end of divButton function
